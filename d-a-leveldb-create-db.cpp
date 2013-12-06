fc959e486f987a5f47bf7895f4fdd3d0e2a87d08
Kiran Pachhai <kiran.pachhai@yahoo.com>
2013-12-06 07:14:03 -0700

    Updated the way it creates the database and the way it searches key values in the database

--- d-a-leveldb-create-db.cpp
+++ d-a-leveldb-create-db.cpp
@@ -12,31 +12,77 @@ using namespace std;
 int main(int argc, char ** argv) {
     ifstream file; 
     string line;
-    char * fileName = argv[1];
-    
-    file.open(fileName);
+
+    string path = "/home/kpachhai/Downloads/KEYVALUEFILES/";
+    string fileNames[26] = {
+	path + "alypaa.txt",
+	path + "carders.cc.txt",
+	path + "elitehacker.txt",
+	path + "english.txt",
+	path + "facebook-first.1.txt",
+	path + "facebook-firstnames.txt",
+	path + "facebook-f.last.txt",
+	path + "facebook-lastnames.txt",
+	path + "facebook-names-unique.txt",
+	path + "facebook-pastebay.txt",
+	path + "faithwriters.txt",
+	path + "fuzzing-strings.txt",
+	path + "german.txt",
+	path + "hak5.txt",
+	path + "honeynet.txt",
+	path + "hotmail.txt",
+	path + "myspace.txt",
+	path + "phpbb.txt",
+	path + "porno.txt",
+	path + "porn-unknown.txt",
+	path + "rockyou.txt",
+	path + "singles.org.txt",
+	path + "tuscl.txt",
+	path + "us_cities.txt"
+    };
+
+    string databaseDir = "./pass_dir"; 
 
     leveldb::DB *db;
     leveldb::Options options;
     options.create_if_missing = true;
-    leveldb::Status status = leveldb::DB::Open(options, "./pass_dir", &db);
+    leveldb::Status status = leveldb::DB::Open(options, databaseDir, &db);
     assert(status.ok());
 
-    leveldb::WriteOptions woptions;
-    
-    while(getline(file, line)) {
-        string pass,hash;
-        istringstream liness(line);
-        getline(liness, pass, ',');
-        getline(liness, hash, ',');
-          
-        leveldb::Slice s = hash;
-        leveldb::Slice t = pass;    
-        db->Put(woptions,s,t);   
+    leveldb::WriteOptions woptions;  
+ 
+    for(int i = 0; i < 26; i++) {
+	    char * filename = new char[fileNames[i].length() + 1];
+	    strcpy(filename, fileNames[i].c_str());
+
+	    file.open(filename);
+	    
+	    while(getline(file, line)) {
+		string pass,hash;
+		istringstream liness(line);
+		getline(liness, pass, ',');
+		getline(liness, hash, ',');
+		  
+		leveldb::Slice s = hash;
+		leveldb::Slice t = pass;    
+		db->Put(woptions,s,t);   
+	    }
+	    file.close();
+	    cout << "Completed loading passwords and hashes from " << filename << endl;
     }
-    file.close();
+
+    int keyValuePairLength = 0;
+    //Read the database
+    leveldb::Iterator* it = db->NewIterator(leveldb::ReadOptions());
+    for (it->SeekToFirst(); it->Valid(); it->Next()) {
+	keyValuePairLength++;
+    }
+    assert(it->status().ok());  // Check for any errors found during the scan
+    delete it;
 
     delete db;
     
+    cout << "Total Key Value Pair in database : " << keyValuePairLength << endl;
+
     return 0;
 }
--- d-a-leveldb.cpp
+++ d-a-leveldb.cpp
@@ -3,27 +3,80 @@
 #include <cassert>
 #include <string>
 #include <iostream>
+#include <cstdlib>
+#include <fstream>
+#include <ostream>
 #include <sstream>
 #include "leveldb/db.h"
 
 using namespace std;
 
+struct hashes
+{
+    string hash;
+    string pass;
+};
+
+void readHashFile(hashes * hToCheck, char * fileName ) {
+    ifstream file; 
+    string line;
+    int i = 0;
+    
+    file.open(fileName);	//passlist/hashFileToTest.txt
+    while(getline(file, line)) {
+        string key = "NOT FOUND";
+	string value = line;            
+		
+        hToCheck[i].pass = key;      
+        hToCheck[i].hash = value;  
+        i++;       
+    }
+    file.close();    
+}
+
+void writeFile(hashes * hToCheck, char * fileName, int nLinesHFile) { 
+    int i;
+    ofstream  fileToWriteTo;
+    fileToWriteTo.open(fileName); //passlist/convertedHash.txt    
+    string compareString = "NOT FOUND";
+    for(i = 0; i < nLinesHFile; i++) {
+    	if(hToCheck[i].pass.compare(compareString)) {
+    		fileToWriteTo << hToCheck[i].pass << ", " << hToCheck[i].hash << endl;
+    	}
+    }
+    fileToWriteTo.close();
+}
+
 int main(int argc, char ** argv) {
     string line;
-    string hashValue = argv[1];
+    char * HFile = argv[1];
+    int nLinesHFile = atoi(argv[2]);
+    char * outputFile = argv[3];
+    int i;
+
+    hashes hToCheck[nLinesHFile];
+    string databaseDir = "./pass_dir";
 
     leveldb::DB *db;
     leveldb::Options options;
     options.create_if_missing = true;
-    leveldb::Status status = leveldb::DB::Open(options, "./pass_dir", &db);
+    leveldb::Status status = leveldb::DB::Open(options, databaseDir, &db);
     assert(status.ok());
 
     leveldb::ReadOptions roptions;
+    leveldb::Status s;
 
+    readHashFile(hToCheck, HFile);
     string password;
-    leveldb::Status s = db->Get(roptions, hashValue, &password);
-    if(s.ok()) cout << "The password is " << password << endl;
-    else cout << "Couldn't match the password in the dictionary" << endl;
+    for(i = 0; i < nLinesHFile; i++) {
+	s = db->Get(roptions, hToCheck[i].hash, &password);
+	if(s.ok()) {
+     	    hToCheck[i].pass = password;
+	}
+    }
+
+    writeFile(hToCheck, outputFile, nLinesHFile);
+
     delete db;   
     
     return 0;
